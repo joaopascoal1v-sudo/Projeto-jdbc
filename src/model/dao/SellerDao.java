6936605254e01dfb77c5cc5a9b8791cd9fcad315
Joao Pascoal <joaopascol1v@gamil.com>
2026-09-08 16:50:05 -0300

    insert implementation

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -7,7 +7,7 @@ import model.entities.Seller;
 
 public interface SellerDao {
 	
-	void isert(Seller obj);
+	void insert(Seller obj);
 	void update(Seller obj);
 	void deleteByid(Integer id);
 	Seller findById(Integer id);
